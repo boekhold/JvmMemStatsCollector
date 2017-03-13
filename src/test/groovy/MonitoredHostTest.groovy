6b02a1c798e2440d5acd4ffea4c22ea492f11cae
Maarten Boekhold <boekhold@gmail.com>
2017-03-13 21:29:22 +0400

    Extend test case to further test the features of 'MonitoredVm''

--- src/test/groovy/MonitoredHostTest.groovy
+++ src/test/groovy/MonitoredHostTest.groovy
@@ -1,19 +1,41 @@
+import org.junit.Ignore
 import org.junit.Test
 import sun.jvmstat.monitor.HostIdentifier
+import sun.jvmstat.monitor.IntegerMonitor
+import sun.jvmstat.monitor.LongMonitor
 import sun.jvmstat.monitor.MonitoredHost
 import sun.jvmstat.monitor.MonitoredVm
+import sun.jvmstat.monitor.StringMonitor
 import sun.jvmstat.monitor.VmIdentifier
 
 class MonitoredHostTest {
+    @Ignore
     @Test
     void monitoredHostTest() {
         HostIdentifier thisHostId = new HostIdentifier((String)null)
         MonitoredHost monitoredHost = MonitoredHost.getMonitoredHost(thisHostId)
 
         monitoredHost.activeVms().each { id ->
+            // 'id' is the OS PID...
+            println "ID: $id --------------------------------------------------------------------------"
+
             VmIdentifier jvmId = new VmIdentifier("//${id}?mode=r")
             MonitoredVm vm = monitoredHost.getMonitoredVm(jvmId)
-            println vm.findByName("sun.rt.javaCommand").stringValue()
+
+            /**
+             * Interesting "monitors" useful for identifying a specific JVM instance are:
+             * - java.rt.vmArgs
+             * - sun.rt.javaCommand
+             */
+            vm.findByPattern('.*').each { monitor ->
+                if (monitor instanceof StringMonitor)
+                    println "${monitor.name} : ${monitor.stringValue()}"
+                if (monitor instanceof IntegerMonitor)
+                    println "${monitor.name} : ${monitor.intValue()}"
+                if (monitor instanceof LongMonitor)
+                    println "${monitor.name} : ${monitor.longValue()}"
+
+            }
         }
     }
 }
